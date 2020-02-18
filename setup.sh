1d7b73e30620c6c48b374d3901c7bb1c3eaa5ffc
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-02-18 13:11:34 +0100

    Add setup script and requirements

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dockerize=${1:-0}
+
+if ! hash git unzip wget 2>/dev/null; then
+  echo '"git", "unzip" and "wget" are necessary, install them first'
+  exit 1
+fi
+
+# download transformers repo
+git clone https://github.com/m0n0l0c0/transformers
+
+mkdir -p data/race
+wget -q -O data/race/race.tar.gz http://www.cs.cmu.edu/~glai1/data/race/RACE.tar.gz
+cd data/race
+tar xvfz race.tar.gz
+cd -
+
+# pre-installed tensorflow-gpu and cuda
+if [[ $dockerize -eq 0 ]]; then
+  pip install requirements.txt
+else
+  echo "Asked for docker install, no packages to install"
+fi
