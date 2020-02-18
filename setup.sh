cfd0a5df503d0442ea0b50485df1d373bfdd37cb
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-02-18 17:55:15 +0100

    Remove unnecessary log
    Fix typo in requirements

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ if ! hash git unzip wget 2>/dev/null; then
   exit 1
 fi
 
+# upgrade pip
+sudo pip install --upgrade pip
+
 # download transformers repo
 git clone https://github.com/m0n0l0c0/transformers
 cd transformers
@@ -16,12 +19,13 @@ cd -
 mkdir -p data/race
 wget -q -O data/race/race.tar.gz http://www.cs.cmu.edu/~glai1/data/race/RACE.tar.gz
 cd data/race
-tar xvfz race.tar.gz
+echo "Uncompressing race..."
+tar xfz race.tar.gz
 cd -
 
 # pre-installed tensorflow-gpu and cuda
 if [[ $dockerize -eq 0 ]]; then
-  sudo pip install requirements.txt
+  sudo pip install -r requirements.txt
 else
   echo "Asked for docker install, no packages to install"
 fi
