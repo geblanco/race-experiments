2bfb7651b85192b7265e093180f76a2d9d19861f
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-03-02 17:13:08 +0100

    Change experiment to multi bert
    Start script to convert Entrance Exams to RACE dataset format

---
+++ ee_processor.py
@@ -0,0 +1,114 @@
+# process Entrance Exams dataset to RACE-like format
+# sample:
+""" {
+    "test-set": {
+        "topic": {
+            "t_id": "0",
+            "t_name": "Entrance Exam",
+            "reading-test": [
+                {
+                    "r_id": "13",
+                    "doc": {
+                        "d_id": "1",
+                        "$t": "<Document.>"
+                    },
+                    "question": [
+                        {
+                            "q_id": "1",
+                            "q_str": "Question",
+                            "answer": [
+                                {
+                                    "a_id": "1",
+                                    "$t": "<ans 1>"
+                                },
+                                {
+                                    "a_id": "2",
+                                    "correct": "Yes",
+                                    "$t": "<ans 2>"
+                                },
+                                {
+                                    "a_id": "3",
+                                    "$t": "<ans 3>"
+                                },
+                                {
+                                    "a_id": "4",
+                                    "$t": "<ans 4>"
+                                }
+                            ]
+                        },
+                        {...}
+"""
+# ->
+"""
+{
+    "version": 1.0,
+    "language": "spanish",
+    "data": [{
+        "answers": ["B", ...],
+        "options": [
+          ["<ans 1>", "<ans 2>", "<ans 3>", "<ans 4>"],
+          ...
+        ],
+        "questions": [
+          ["q1", ...]
+        ],
+        "article": "<Document>",
+        "id": ""
+    }]
+}
+"""
+
+import json
+import argparse
+
+flags = None
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--input', type=str, required=True,
+    help="Dataset to process")
+  return parser.parse_args()
+
+answer_indices_to_letters = ['A', 'B', 'C', 'D']
+
+def Example(object):
+  def __init__(self, id, ee_example):
+    self.id = id
+    self.article = ee_example['doc']['$t']
+
+    self.questions = self._process_questions(ee_example['question'])
+    self.answers = self._process_answers(ee_example['question'])
+    self.options = self._process_options(ee_example['question'])
+
+  def _process_questions(self, questions):
+    return [q['q_str'] for q in questions]
+
+  def _process_answers(self, questions):
+    # get the index of the answer with correct=True
+    answers = []
+    for question in question:
+      for index, answer in enumerate(question['answer']):
+        if answer['correct']:
+          answers.append(answer_indices_to_letters[index])
+    return answers
+
+  def _process_options(self, questions):
+    options = []
+    for question in questions:
+      options.append([q['$t'] for q in question['answer']]):
+    return options
+
+  def to_json(self):
+    return self.__dict__
+
+def process_example(example):
+
+
+def main():
+  data = json.load(open(flags.input))
+
+
+if __name__ == '__main__':
+  flags = parse_args()
+  main()
+
--- run_experiment.sh
+++ run_experiment.sh
@@ -1,21 +1,25 @@
 export RACE_DIR=data/RACE
+export BERT_MODEL=multi_cased_L-12_H-768_A-12
+export BERT_MULTI_DIR=models/$BERT_MODEL
+
 python3 ./transformers/examples/run_multiple_choice.py \
   --model_type albert \
   --task_name race \
-  --model_name_or_path albert-large-v2 \
+  --model_name_or_path $BERT_MULTI_DIR/bert_model.ckpt \
+  --config_name $BERT_MULTI_DIR/bert_config.json \
+  --tokenizer_name $BERT_MULTI_DIR/vocab.txt \
   --data_dir $RACE_DIR \
-  --output_dir data/albert-large-v2 \
-  --config_name config/albert_large_config.json \
+  --output_dir data/$BERT_MODEL \
   --overwrite_output \
   --do_train \
   --do_eval \
-  --do_lower_case \
   --fp16 \
+  --fp16_opt_level "O2" \
   --learning_rate 1e-5 \
   --num_train_epochs 3 \
-  --max_seq_length 512 \
-  --per_gpu_train_batch_size=32 \
+  --max_seq_length 256 \
+  --per_gpu_train_batch_size=2 \
   --per_gpu_eval_batch_size=8 \
-  --gradient_accumulation_steps 2 \
+  --gradient_accumulation_steps 64 \
   --warmup_steps 1000
 
