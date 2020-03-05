722e2c4c53cccc1075dd7b54f8aa1b219da5cf19
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-03-05 13:44:48 +0100

    Fix parameters handling in runner script

--- runner.sh
+++ runner.sh
@@ -15,19 +15,60 @@ echo "###### Starting experiments $(date)"
 total_start_time=$(date -u +%s)
 
 args="./transformers/examples/run_multiple_choice.py "
-args+="--model_type $MODEL_TYPE "
-args+="--task_name $TASK_NAME "
-args+="--model_name_or_path $MODEL_NAME_OR_PATH "
-args+="--data_dir $DATA_DIR "
-args+="--data_id $DATA_ID "
-args+="--output_dir $OUTPUT_DIR "
-args+="--overwrite_output "
-args+="--learning_rate 1e-5 "
-args+="--num_train_epochs 3 "
-args+="--max_seq_length 256 "
-args+="--per_gpu_train_batch_size=2 "
-args+="--per_gpu_eval_batch_size=8 "
-args+="--gradient_accumulation_steps 64 "
+
+# this options should be automagic (maybe do it by jsonnet)
+ 
+if [[ ! -z "$MODEL_TYPE" ]];then
+  args+="--model_type $MODEL_TYPE "
+fi
+
+if [[ ! -z "$TASK_NAME" ]];then
+  args+="--task_name $TASK_NAME "
+fi
+
+if [[ ! -z "$MODEL_NAME_OR_PATH" ]];then
+  args+="--model_name_or_path $MODEL_NAME_OR_PATH "
+fi
+
+if [[ ! -z "$DATA_DIR" ]];then
+  args+="--data_dir $DATA_DIR "
+fi
+
+if [[ ! -z "$DATA_ID" ]];then
+  args+="--data_id $DATA_ID "
+fi
+
+if [[ ! -z "$OUTPUT_DIR" ]];then
+  args+="--output_dir $OUTPUT_DIR "
+fi
+
+if [[ ! -z "$LEARNING_RATE" ]];then
+  args+="--learning_rate $LEARNING_RATE "
+fi
+
+if [[ ! -z "$NUM_TRAIN_EPOCHS" ]];then
+  args+="--num_train_epochs $NUM_TRAIN_EPOCHS "
+fi
+
+if [[ ! -z "$MAX_SEQ_LENGTH" ]];then
+  args+="--max_seq_length $MAX_SEQ_LENGTH "
+fi
+
+if [[ ! -z "$PER_GPU_TRAIN_BATCH_SIZE" ]];then
+  args+="--per_gpu_train_batch_size $PER_GPU_TRAIN_BATCH_SIZE "
+fi
+
+if [[ ! -z "$PER_GPU_EVAL_BATCH_SIZE" ]];then
+  args+="--per_gpu_eval_batch_size $PER_GPU_EVAL_BATCH_SIZE "
+fi
+
+if [[ ! -z "$GRADIENT_ACCUMULATION_STEPS" ]];then
+  args+="--gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS "
+fi
+
+if [[ ! -z "$OVERWRITE_OUTPUT" ]]; then
+  args+="--overwrite_output "
+fi
 
 if [[ ! -z "$DO_TRAIN" ]]; then
   args+="--do_train "
