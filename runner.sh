57d4a0fafad09251032a21b4388484a2968321ee
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-03-16 11:20:11 +0100

    Add support for multiple experiments to runner script
    Better handle results in runner script

--- runner.sh
+++ runner.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
 
-if [[ "$#" -lt 1 ]]; then
-  echo "Usage runner.sh <experiment file>"
-  exit 0
-fi
+result_files=(
+  "is_test_false_eval_results.txt"
+  "is_test_false_eval_preds.json"
+  "is_test_true_eval_results.txt"
+  "is_test_true_eval_preds.json"
+)
 
-file=$1
+save_experiment_data() {
+  local model_dir=$1; shift
+  local results_dir=$1; shift
+  local exp_name=$1; shift
+  for file in ${result_files[@]}; do
+    if [[ -f $model_dir/$file ]]; then
+      echo "Saving $model_dir/$file $results_dir/${exp_name}_${file}"
+      mv $model_dir/$file $results_dir/${exp_name}_${file}
+    fi
+  done
+}
+
+run_experiment() {
+  local file=$1; shift
+  args="./transformers/examples/run_multiple_choice.py "
+  for line in $(sed -n 's/^export \(.*\)=\([^ ]*\)/\1=\2/p' $file); do 
+    # <key>=<value>
+    key=${line%=*}
+    value=${line#*=}
+    # lowercase
+    args+="--${key,,} "
+    if [[ ! "${value,,}" == "true" ]]; then
+      args+="$value "
+    fi
+  done
+
+  python3 ${args[@]}
+}
+
+results_dir='./results'
+[[ ! -d $results_dir ]] && mkdir $results_dir
 
 echo "###### Starting experiments $(date)"
 total_start_time=$(date -u +%s)
 
-args="./transformers/examples/run_multiple_choice.py "
-for line in $(sed -n 's/^export \(.*\)=\([^ ]*\)/\1=\2/p' $file); do 
-  # <key>=<value>
-  key=${line%=*}
-  value=${line#*=}
-  # lowercase
-  args+="--${key,,} "
-  if [[ ! "${value,,}" == "true" ]]; then
-    args+="$value "
-  fi
+experiments=($@)
+for exp in ${experiments[@]}; do
+  model_dir=$(sed -n 's/export OUTPUT_DIR=\(.*\)/\1/p' experiments/$exp);
+  exp_name=${exp%.*}
+  echo "*********** $exp *************";
+  run_experiment experiments/$exp
+  save_experiment_data $model_dir $results_dir $exp_name
+  echo "********************************";
 done
-
-python3 ${args[@]}
 
 total_end_time=$(date -u +%s)
 total_elapsed=$(python3 -c "print('{:.2f}'.format(($total_end_time - $total_start_time)/60.0 ))")
