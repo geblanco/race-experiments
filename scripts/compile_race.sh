f1753638b8b2e305684fd9225aaf6afe25ab2970
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-04-21 03:46:19 +0200

    Add race compilation as dvc stage, used for evalution

---
+++ scripts/compile_race.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RACE=${1:-"data/RACE"}
+OUTPUT=${2:-$RACE}
+
+if [[ ! -d $OUTPUT ]]; then
+  mkdir -p $OUTPUT
+fi
+
+for data_set in 'test' 'dev' 'train'; do
+  for partition in 'high' 'middle'; do
+    compiled_dataset="${OUTPUT}/race_${data_set}_compiled_${partition}.json"
+    echo "-> Compiling ${data_set} - ${partition} to $compiled_dataset"
+    python scripts/utils_race.py --data ${RACE}/${data_set} --partition ${partition} > $compiled_dataset
+  done
+done
+
